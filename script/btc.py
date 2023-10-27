29ba954410a30327358d3fb640a27e9209151fe0
Hach05 <tech.hach@outlook.com>
2023-10-27 17:38:02 +0900

    BTCの株価グラフ(一ヵ月)を表示する"/graph"コマンドを追加

--- script/btc.py
+++ script/btc.py
@@ -2,31 +2,32 @@ import matplotlib.pyplot as plt
 import requests
 import pandas as pd
 
-# CoinGecko APIを使用してBTC価格データを取得
-url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
-params = {
-    "vs_currency": "usd",  # 価格の通貨をUSDに設定
-    "days": 30  # 取得する日数を設定
-}
+def get_btc_graph():
+    # CoinGecko APIを使用してBTC価格データを取得
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+    params = {
+        "vs_currency": "usd",  # 価格の通貨をUSDに設定
+        "days": 30  # 取得する日数を設定
+    }
 
-response = requests.get(url, params=params)
-data = response.json()
+    response = requests.get(url, params=params)
+    data = response.json()
 
-# データをDataFrameに変換
-df = pd.DataFrame(data["prices"], columns=["timestamp", "price"])
-df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+    # データをDataFrameに変換
+    df = pd.DataFrame(data["prices"], columns=["timestamp", "price"])
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
 
 
-# グラフの設定
-plt.figure(figsize=(12, 6))
-plt.plot(df["timestamp"], df["price"], label="BTC Price (USD)", color="blue")
-plt.title("BTC Price Over the Last 30 Days")
-plt.xlabel("Date")
-plt.ylabel("Price (USD)")
-plt.grid(True)
+    # グラフの設定
+    plt.figure(figsize=(12, 6))
+    plt.plot(df["timestamp"], df["price"], label="BTC Price (USD)", color="blue")
+    plt.title("BTC Price Over the Last 30 Days")
+    plt.xlabel("Date")
+    plt.ylabel("Price (USD)")
+    plt.grid(True)
 
-# グラフを表示
-plt.legend()
+    # グラフを表示
+    plt.legend()
 
-plt.savefig("img/BTC_30days.png")
+    plt.savefig("img/BTC_price.png")
 
--- script/main.py
+++ script/main.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 import os
 from dotenv import load_dotenv
+import btc
 
 load_dotenv()
 
@@ -11,11 +12,63 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+
+def check_is_me(ctx: discord.Interaction):
+    return ctx.user.id == 580716525690945538
+
+
+# 参加しているサーバー表示
+@tree.command(
+    name="gl",
+    description="hadadayoだけが動かせるコマンド"
+)
+@app_commands.check(check_is_me)
+async def showguild(ctx: discord.Interaction):
+    count_server = len(client.guilds)
+    guildlist = discord.Embed(title="参加しているサーバーList", description=f"合計：{count_server}サーバー")
+    for guild in client.guilds:
+        guildlist.add_field(name="サーバー名：", value=f"{guild}", inline=False)
+    await ctx.response.send_message(embed=guildlist, ephemeral=True)
+
+
+@showguild.error
+async def showguild_error(ctx: discord.Interaction, error):
+    await ctx.response.send_message(f"{ctx.user.mention}さんは権限がありません")
+
+
 @client.event
 async def on_ready():
-    print("-----------------------")
-    print("Bot is Ready")
-    print("-----------------------")
+    await client.change_presence(activity=discord.Game(name="HADABot"))
+    print("-----------------------------------")
+    print("Bot is ready.")
+    print("-----------------------------------")
     await tree.sync()
+    print("Syncを実行しました")
+    print("-----------------------------------")
+
+@client.event
+async def on_guild_join():
+    count_server = len(client.guilds)
+    print("-----------------------------------")
+    print('新しくサーバーに追加されました。')
+    print(f"現在参加しているサーバー数：{count_server}")
+    print("-----------------------------------")
+    for guild in client.guilds:
+        print(guild.name)
+    print("-----------------------------------")
+
+@tree.command(name="test", description="testです")
+async def test_command(interaction: discord.Interaction):
+    await interaction.response.send_message("test!", ephemeral=True)
+
+@tree.command(name="graph", description="グラフを表示します。")
+async def graph(interaction: discord.Interaction):
+    btc.get_btc_graph()
+    embed=discord.Embed(title="BTC(直近1ヵ月)")
+    file = discord.File(f"img/BTC_price.png",f"BTC_price.png")
+    embed.set_image(url="attachment://BTC_price.png")
+    await interaction.response.send_message(embed=embed, file=file)
+
+
 
 client.run(TOKEN)
