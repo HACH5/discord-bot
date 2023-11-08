37b43bc73769a6ee79d3830321dee64471111c84
Hach05 <tech.hach@outlook.com>
2023-11-08 18:39:18 +0900

    chat機能の更新->openaiのgpt-3.5-turboを使用

--- script/main.py
+++ script/main.py
@@ -1,12 +1,16 @@
-import discord
+import discord, asyncio
 from discord import app_commands
 import os, datetime
 from dotenv import load_dotenv
 import btc
+import openai
+import time
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_engine = "gpt-3.5-turbo"
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -15,26 +19,6 @@ tree = app_commands.CommandTree(client)
 
 def check_is_me(ctx: discord.Interaction):
     return ctx.user.id == int(os.getenv("DISCORD_ID"))
-
-
-# 参加しているサーバー表示
-@tree.command(
-    name="gl",
-    description="hadadayoだけが動かせるコマンド"
-)
-@app_commands.check(check_is_me)
-async def showguild(ctx: discord.Interaction):
-    count_server = len(client.guilds)
-    guildlist = discord.Embed(title="参加しているサーバーList", description=f"合計：{count_server}サーバー")
-    for guild in client.guilds:
-        guildlist.add_field(name="サーバー名：", value=f"{guild}", inline=False)
-    await ctx.response.send_message(embed=guildlist, ephemeral=True)
-
-
-@showguild.error
-async def showguild_error(ctx: discord.Interaction, error):
-    await ctx.response.send_message(f"{ctx.user.mention}さんは権限がありません")
-
 
 @client.event
 async def on_ready():
@@ -57,6 +41,25 @@ async def on_guild_join():
         print(guild.name)
     print("-----------------------------------")
 
+# 参加しているサーバー表示
+@tree.command(
+    name="gl",
+    description="hadadayoだけが動かせるコマンド"
+)
+
+@app_commands.check(check_is_me)
+async def showguild(ctx: discord.Interaction):
+    count_server = len(client.guilds)
+    guildlist = discord.Embed(title="参加しているサーバーList", description=f"合計：{count_server}サーバー")
+    for guild in client.guilds:
+        guildlist.add_field(name="サーバー名：", value=f"{guild}", inline=False)
+    await ctx.response.send_message(embed=guildlist, ephemeral=True)
+
+
+@showguild.error
+async def showguild_error(ctx: discord.Interaction, error):
+    await ctx.response.send_message(f"{ctx.user.mention}さんは権限がありません")
+
 @tree.command(name="test", description="testです")
 async def test_command(interaction: discord.Interaction):
     await interaction.response.send_message("test!", ephemeral=True)
@@ -68,6 +71,31 @@ async def graph(interaction: discord.Interaction):
     file = discord.File(f"img/BTC_price.png",f"BTC_price.png")
     embed.set_image(url="attachment://BTC_price.png")
     await interaction.response.send_message(embed=embed, file=file)
+
+@tree.command(name="chat", description="お話をしましょう！")
+async def chat(interaction: discord.Interaction, text:str):
+    try:
+        await interaction.response.defer()
+        completion = openai.ChatCompletion.create(
+            model=model_engine,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "日本語で返答"
+                },
+                {
+                    "role": "user",
+                    "content": text
+                }
+            ],
+        )
+        res = completion["choices"][0]["message"]["content"]
+        await interaction.followup.send(res)
+    except:
+        import traceback
+        traceback.print_exc()
+        await interaction.response.send_message("エラーが発生しました")
+
 
 
 client.run(TOKEN)
