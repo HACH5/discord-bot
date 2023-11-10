31910c3ebe9dce1001146dd784d2f2defefdb93a
Hach05 <tech.hach@outlook.com>
2023-11-10 17:33:10 +0900

    ・各コマンドの説明部分を変更。 ・chatbot.pyの削除

--- script/main.py
+++ script/main.py
@@ -30,17 +30,6 @@ async def on_ready():
     print("Syncを実行しました")
     print("-----------------------------------")
 
-@client.event
-async def on_guild_join():
-    count_server = len(client.guilds)
-    print("-----------------------------------")
-    print('新しくサーバーに追加されました。')
-    print(f"現在参加しているサーバー数：{count_server}")
-    print("-----------------------------------")
-    for guild in client.guilds:
-        print(guild.name)
-    print("-----------------------------------")
-
 # 参加しているサーバー表示
 @tree.command(
     name="gl",
@@ -60,11 +49,11 @@ async def showguild(ctx: discord.Interaction):
 async def showguild_error(ctx: discord.Interaction, error):
     await ctx.response.send_message(f"{ctx.user.mention}さんは権限がありません")
 
-@tree.command(name="test", description="testです")
-async def test_command(interaction: discord.Interaction):
-    await interaction.response.send_message("test!", ephemeral=True)
+# @tree.command(name="test", description="testです")
+# async def test_command(interaction: discord.Interaction):
+#     await interaction.response.send_message("test!", ephemeral=True)
 
-@tree.command(name="graph", description="グラフを表示します。")
+@tree.command(name="graph", description="BTCの直近(1ヵ月)のグラフを表示します。")
 async def graph(interaction: discord.Interaction):
     btc.get_btc_graph()
     embed=discord.Embed(title="BTC(直近1ヵ月)")
@@ -72,7 +61,7 @@ async def graph(interaction: discord.Interaction):
     embed.set_image(url="attachment://BTC_price.png")
     await interaction.response.send_message(embed=embed, file=file)
 
-@tree.command(name="chat", description="お話をしましょう！")
+@tree.command(name="chat", description="お話をしましょう！textの部分に文章を入力してください。")
 async def chat(interaction: discord.Interaction, text:str):
     try:
         await interaction.response.defer()
