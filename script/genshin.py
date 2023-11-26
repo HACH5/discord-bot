ad042aa2a6e7978fdd4002031ed4624bc71181b6
Hach <55661562+HACH5@users.noreply.github.com>
2023-11-27 07:22:59 +0900

    Update genshin.py

--- script/genshin.py
+++ script/genshin.py
@@ -1,5 +1,22 @@
 import sqlite3
 
+def element_color(element_name):
+    match element_name:
+        case '炎':
+            return '#ff0000'
+        case '水':
+            return '#0000ff'
+        case '氷':
+            return '#a0d8ef'
+        case '雷':
+            return '#800080'
+        case '風':
+            return '#bee0c2'
+        case '草':
+            return '#008000'
+        case '岩':
+            return '#ffa500'
+
 def search_name_artifact(name):
     con = sqlite3.connect("database/genshin.db")
     cur = con.cursor()
@@ -48,6 +65,38 @@ def search_element_name(element_id):
     con.close()
     return r
 
+def search_artifact_id(artifact_name):
+    con = sqlite3.connect("database/genshin.db")
+    cur = con.cursor()
+    cur.execute(f"select artifact_id from artifact where artifact_ja_name like '%{artifact_name}%'")
+    r = cur.fetchall()
+    con.close()
+    return r
+
+def search_chara_artifact_name(name):
+    n = search_name_character(name)
+    con = sqlite3.connect("database/genshin.db")
+    cur = con.cursor()
+    cur.execute(f"""select chara_ja_name, rarity, element_ja_name, type_ja_name, artifact_ja_name, ja_best_main_sand_status, ja_best_main_goblet_status, ja_best_main_crown_status, 
+                from artifact_character natural join character_type natural join character 
+                natural join artifact natural join element
+                where chara_id = {n[0][0]}""")
+    r = cur.fetchall()
+    con.close()
+    return r
+
+def search_chara_artifact_artifact(artifact):
+    id = search_artifact_id(artifact)
+    con = sqlite3.connect("database/genshin.db")
+    cur = con.cursor()
+    cur.execute(f"""select chara_ja_name, rarity, element_ja_name, type_ja_name, artifact_ja_name, ja_best_main_sand_status, ja_best_main_goblet_status, ja_best_main_crown_status
+                from artifact_character natural join character_type natural join character 
+                natural join artifact natural join element
+                where artifact_id = {id[0][0]}""")
+    r = cur.fetchall()
+    con.close()
+    return r
+
 if __name__ == '__main__':
-    res = search_birthday_character("09")
+    res = search_chara_artifact_artifact("林")
     print(res)
